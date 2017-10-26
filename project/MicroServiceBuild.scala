37a289212827d4dffed392a5040752c31a4ad43a
mohammed.abidi <mohammed.abidi@digital.hmrc.gov.uk>
2017-10-26 19:22:24 +0100

    TAVC-4219 Library Updates

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -12,8 +12,8 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  private val microserviceBootstrapVersion = "6.10.0"
-  private val playAuthVersion = "4.3.0"
+  private val microserviceBootstrapVersion = "6.11.0"
+  private val playAuthVersion = "5.1.0"
   private val playHealthVersion = "2.1.0"
   private val logbackJsonLoggerVersion = "3.1.0"
   private val playUrlBindersVersion = "2.1.0"
